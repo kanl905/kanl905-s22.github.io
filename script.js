cbb64ff9a6e883017c4dcb2cc5c801a36d87be19
Glitch (jasminekan) <none>
2022-03-24 23:08:53 +0000

    🦗📗 Updated with Glitch

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 /* pop ups */
 $("#one").click(function() {
     $("#oneinfo").toggle('blue black');
+    $('#twoinfo,#threeinfo,#fourinfo').hide('hello');
 });
 
   $("#two").click(function() {
     $("#twoinfo").toggle('blue black');
+    $('#oneinfo,#threeinfo,#fourinfo').hide('hello');
 });
 
   $("#three").click(function() {
     $("#threeinfo").toggle('blue black');
+    $('#twoinfo,#oneinfo,#fourinfo').hide('hello');
 });
 
   $("#four").click(function() {
     $("#fourinfo").toggle('blue black');
+    $('#twoinfo,#threeinfo,#oneinfo').hide('hello');
 });
 
 /* colour */
 
-$("#one").click(function(){
-    $('#one').toggleClass('blue');
-    $('#oneinfo').toggleClass('blue');
-});
+        /*$("#one").click(function(){
+            $('#one').toggleClass('blue');
+        });
 
-$("#two").click(function(){
-    $('#two').toggleClass('blue');
-    $('#twoinfo').toggleClass('blue');
-});
+        $("#two").click(function(){
+            $('#two').toggleClass('blue');
+        });
 
-$("#three").click(function(){
-    $('#three').toggleClass('blue');
-    $('#threeinfo').toggleClass('blue');
-});
+        $("#three").click(function(){
+            $('#three').toggleClass('blue');
+        });
 
-$("#four").click(function(){
-    $('#four').toggleClass('blue');
-    $('#fourinfo').toggleClass('blue');
-});
+        $("#four").click(function(){
+            $('#four').toggleClass('blue');
+        });
 
+let number=document.getElementById("oneinfo");
+if (number.style.display=='block') {
+   document.getElementById("one").style.color="black";
+} else {
+  document.getElementById("one").style.color="blue";
+}*/
