9266614708815162495b52cab865733980d96e2f
Glitch (jasminekan) <none>
2022-03-25 02:03:42 +0000

    🕙🎭 Updated with Glitch

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 /* pop ups */
 $("#one").click(function() {
+    $(this).toggleClass("blueclick");
     $("#oneinfo").toggle('blue black');
     $('#twoinfo,#threeinfo,#fourinfo').hide('hello');
 });
 
   $("#two").click(function() {
+    $(this).toggleClass("blueclick");
     $("#twoinfo").toggle('blue black');
     $('#oneinfo,#threeinfo,#fourinfo').hide('hello');
 });
 
   $("#three").click(function() {
+    $(this).toggleClass("blueclick");
     $("#threeinfo").toggle('blue black');
     $('#twoinfo,#oneinfo,#fourinfo').hide('hello');
 });
 
   $("#four").click(function() {
+    $(this).toggleClass("blueclick");
     $("#fourinfo").toggle('blue black');
     $('#twoinfo,#threeinfo,#oneinfo').hide('hello');
 });
